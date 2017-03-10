5f53e6a62fa1d02d4a3ddc1d22adfdec44653449
Sasha Cuerda <sasha.cuerda@gmail.com>
2017-03-10 17:28:10 -0500

    slight refactoring of async code. will probably pull many of those functions into a separate helper file. Writing tests against these helpers.

--- ctdata_edsight_scraping_tool/fetch_async.py
+++ ctdata_edsight_scraping_tool/fetch_async.py
@@ -30,25 +30,44 @@ def build_params_list(dataset, base_qs, variables):
         params.append(new_qs)
     return params
 
+def get_xpaths(filters, variables):
+    return [f['xpath_id'] for f in filters if f['name'] in variables]
+
+
+def build_url_list(params, xpaths, url, output_dir, dataset_name):
+    """Build up a list of target objects."""
+    targets = []
+    for p in params:
+        # In testing we have a basic param object, but in actual work it is more complex
+        # and includes params that are only specific to the SAS stored procedure. We don't need
+        # these for the file naming, which is why we use the xpath lookup to pull out the subset
+        f = [p[v] for v in xpaths]
+        filename_variables = '_'.join(f)
+        slug = filename_variables.replace('/', '-').replace(' ', '-')
+        filename = "{}{}_{}.csv".format(output_dir, dataset_name, slug)
+        targets.append({'url': url, 'param': p, 'filename': filename})
+    return targets
 
 def setup_download_targets(dataset, output_dir, variable):
     """Download the csv file of the dataset to a target directory."""
     ds = links[dataset]
     ds_filters = ds['filters']
-    xpaths = [f['xpath_id'] for f in ds_filters if f['name'] in variable]
     dl_link = ds['download_link']
+
+    # Parse the link url, extract the basic params and then reset the url to its root
     dl_parsed = urlparse(dl_link)
     qs = parse_qs(dl_parsed.query)
     new_url = dl_parsed._replace(query=None).geturl()
+
+    # Call helper function to extract the correct xpaths from our lookup
+    xpaths = get_xpaths(ds_filters, variable)
+
+    # Build up a list params for each variable combo
     params = build_params_list(ds, qs, variable)
-    targets = []
-    for p in params:
-        f = [p[v] for v in xpaths]
-        filename_variables = '_'.join(f)
-        slug = filename_variables.replace('/', '-').replace(' ', '-')
-        filename = "{}{}_{}.csv".format(output_dir, dataset, slug)
-        targets.append({'url': new_url, 'param': p, 'filename': filename})
-    return targets
+
+    # Return a list of objects that can be past to our http request
+    # generator to build up a final url with params
+    return build_url_list(params, xpaths, new_url, output_dir, dataset)
 
 
 async def get_report(url, params, file):
--- tests/test_ctdata_edsight_scraping_tool.py
+++ tests/test_ctdata_edsight_scraping_tool.py
@@ -13,29 +13,103 @@ import pytest
 from contextlib import contextmanager
 from click.testing import CliRunner
 
-from ctdata_edsight_scraping_tool import ctdata_edsight_scraping_tool
-from ctdata_edsight_scraping_tool import cli
-
 
 @pytest.fixture
-def response():
-    """Sample pytest fixture.
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+def dataset():
+    return {
+        "dataset": "Chronic Absenteeism",
+        "link": "http://edsight.ct.gov/SASStoredProcess/do?_program=%2FCTDOE%2FEdSight%2FRelease%2FReporting%2FPublic%2FReports%2FStoredProcesses%2FChronicAbsenteeismReport&_select=Submit&_parampass=Yes",
+        "download_link": "http://edsight.ct.gov/SASStoredProcess/do?_program=/CTDOE/EdSight/Release/Reporting/Public/Reports/StoredProcesses/ChronicAbsenteeismExport&_year=2015-16&_district=+&_subgroup=All+Students&_school=+",
+        "filters": [
+            {
+                "name": "Year",
+                "xpath_id": "_year",
+                "options": ["Trend", "2015-16", "2014-15", "2013-14", "2012-13", "2011-12"]
+            },
+            {
+                "name": "District",
+                "xpath_id": "_district",
+                "options": ["State of Connecticut", "All Districts"]
+            },
+            {
+                "name": "School",
+                "xpath_id": "_school",
+                "options": ["All Schools"]
+            }, {
+                "name": "Filter By",
+                "xpath_id": "_subgroup",
+                "options": ["All Students", "Race/Ethnicity", "Gender", "Special Education Status",
+                            "Free/Reduced Price Meal Eligibility", "English Learner Status", "Grade"]
+            }]
+    }
 
-
-def test_content(response):
+def test_single_var_params_list_generation(dataset):
     """Sample pytest test function with the pytest fixture as an argument.
     """
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
-def test_command_line_interface():
-    runner = CliRunner()
-    result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert 'ctdata_edsight_scraping_tool.cli.main' in result.output
-    help_result = runner.invoke(cli.main, ['--help'])
-    assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
+    from ctdata_edsight_scraping_tool.fetch_async import build_params_list
+    params = build_params_list(dataset, {}, ['Year'])
+    assert params == [{'_year': 'Trend'}, {'_year': '2015-16'}, {'_year': '2014-15'}, {'_year': '2013-14'}, {'_year': '2012-13'}, {'_year': '2011-12'}]
+
+
+def test_two_var_params_list_generation(dataset):
+    """Sample pytest test function with the pytest fixture as an argument.
+    """
+    from ctdata_edsight_scraping_tool.fetch_async import build_params_list
+    params = build_params_list(dataset, {}, ['Year', 'Filter By'])
+    assert params == [
+        {'_year': 'Trend', '_subgroup': 'All Students'},
+        {'_year': 'Trend', '_subgroup': 'Race/Ethnicity'},
+        {'_year': 'Trend', '_subgroup': 'Gender'}, {'_year': 'Trend', '_subgroup': 'Special Education Status'},
+        {'_year': 'Trend', '_subgroup': 'Free/Reduced Price Meal Eligibility'},
+        {'_year': 'Trend', '_subgroup': 'English Learner Status'}, {'_year': 'Trend', '_subgroup': 'Grade'},
+        {'_year': '2015-16', '_subgroup': 'All Students'}, {'_year': '2015-16', '_subgroup': 'Race/Ethnicity'},
+        {'_year': '2015-16', '_subgroup': 'Gender'}, {'_year': '2015-16', '_subgroup': 'Special Education Status'},
+        {'_year': '2015-16', '_subgroup': 'Free/Reduced Price Meal Eligibility'},
+        {'_year': '2015-16', '_subgroup': 'English Learner Status'}, {'_year': '2015-16', '_subgroup': 'Grade'},
+        {'_year': '2014-15', '_subgroup': 'All Students'}, {'_year': '2014-15', '_subgroup': 'Race/Ethnicity'},
+        {'_year': '2014-15', '_subgroup': 'Gender'}, {'_year': '2014-15', '_subgroup': 'Special Education Status'},
+        {'_year': '2014-15', '_subgroup': 'Free/Reduced Price Meal Eligibility'},
+        {'_year': '2014-15', '_subgroup': 'English Learner Status'}, {'_year': '2014-15', '_subgroup': 'Grade'},
+        {'_year': '2013-14', '_subgroup': 'All Students'}, {'_year': '2013-14', '_subgroup': 'Race/Ethnicity'},
+        {'_year': '2013-14', '_subgroup': 'Gender'}, {'_year': '2013-14', '_subgroup': 'Special Education Status'},
+        {'_year': '2013-14', '_subgroup': 'Free/Reduced Price Meal Eligibility'},
+        {'_year': '2013-14', '_subgroup': 'English Learner Status'}, {'_year': '2013-14', '_subgroup': 'Grade'},
+        {'_year': '2012-13', '_subgroup': 'All Students'}, {'_year': '2012-13', '_subgroup': 'Race/Ethnicity'},
+        {'_year': '2012-13', '_subgroup': 'Gender'}, {'_year': '2012-13', '_subgroup': 'Special Education Status'},
+        {'_year': '2012-13', '_subgroup': 'Free/Reduced Price Meal Eligibility'},
+        {'_year': '2012-13', '_subgroup': 'English Learner Status'}, {'_year': '2012-13', '_subgroup': 'Grade'},
+        {'_year': '2011-12', '_subgroup': 'All Students'}, {'_year': '2011-12', '_subgroup': 'Race/Ethnicity'},
+        {'_year': '2011-12', '_subgroup': 'Gender'}, {'_year': '2011-12', '_subgroup': 'Special Education Status'},
+        {'_year': '2011-12', '_subgroup': 'Free/Reduced Price Meal Eligibility'},
+        {'_year': '2011-12', '_subgroup': 'English Learner Status'}, {'_year': '2011-12', '_subgroup': 'Grade'}]
+
+
+def test_get_xpaths(dataset):
+    from ctdata_edsight_scraping_tool.fetch_async import get_xpaths
+    xpaths = get_xpaths(dataset['filters'], ['Year', 'Filter By'])
+    assert xpaths == ['_year', '_subgroup']
+
+def test_url_list_builder():
+    from ctdata_edsight_scraping_tool.fetch_async import build_url_list
+
+    xpaths = ['_year', '_subgroup']
+
+    params = [
+        {'_year': 'Trend', '_subgroup': 'All Students'},
+        {'_year': 'Trend', '_subgroup': 'Race/Ethnicity'},
+        {'_year': '2011-12', '_subgroup': 'All Students'},
+        {'_year': '2011-12', '_subgroup': 'Race/Ethnicity'}
+    ]
+
+    targets = [{'url': 'http://test.com', 'param': {'_year': 'Trend', '_subgroup': 'All Students'},
+      'filename': './test_Trend_All-Students.csv'},
+     {'url': 'http://test.com', 'param': {'_year': 'Trend', '_subgroup': 'Race/Ethnicity'},
+      'filename': './test_Trend_Race-Ethnicity.csv'},
+     {'url': 'http://test.com', 'param': {'_year': '2011-12', '_subgroup': 'All Students'},
+      'filename': './test_2011-12_All-Students.csv'},
+     {'url': 'http://test.com', 'param': {'_year': '2011-12', '_subgroup': 'Race/Ethnicity'},
+      'filename': './test_2011-12_Race-Ethnicity.csv'}]
+
+    outputs = build_url_list(params, xpaths, 'http://test.com', './', 'test')
+
+    assert outputs == targets

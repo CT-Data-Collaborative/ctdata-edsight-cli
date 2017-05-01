384d850f4dd48d17784a721fe12ee888a50c1113
Sasha Cuerda <sasha.cuerda@gmail.com>
2017-05-01 11:44:56 -0400

    disable progress bar

--- ctdata_edsight_scraping_tool/fetch_sync.py
+++ ctdata_edsight_scraping_tool/fetch_sync.py
@@ -32,39 +32,40 @@ def fetch_sync(dataset, output_dir, geography, catalog, save=True):
         s.get(BASE_URL)
 
         click.echo("Fetching {}\n\n".format(dataset))
-        with progressbar.ProgressBar(max_value=len(targets)) as bar:
-            for i, t in enumerate(targets):
-                bar.update(i)
-                target_url_query = urllib.parse.urlencode(t['param']).replace('%2F', '/')
+        # with progressbar.ProgressBar(max_value=len(targets)) as bar:
+        for i, t in enumerate(targets):
+            bar.update(i)
+            target_url_query = urllib.parse.urlencode(t['param']).replace('%2F', '/')
 
-                # click.echo("\n\nDownloading: {}\nFrom: {}?{}".format(os.path.basename(t['filename']),
-                #                                                         t['url'],target_url_query))
+            # click.echo("\n\nDownloading: {}\nFrom: {}?{}".format(os.path.basename(t['filename']),
+            #                                                         t['url'],target_url_query))
 
-                ATTEMPTS = 0
-                STATUS_CODE = 0
-                data = '<html>'
-                while ATTEMPTS < 4 and STATUS_CODE != 200 and data.find('<html>') != -1:
-                    try:
-                        response = s.get(t['url'], params=t['param'])
-                    except Exception as e:
-                        click.echo(e)
-                        STATUS_CODE = 0
-                        continue
-                    STATUS_CODE = response.status_code
-                    data = response.text
-                    target_url = response.url
-                if save and STATUS_CODE == 200:
-                    # Lets check to make sure that the content is an actual CSV files with results
-                    no_results = data.find('The query you have run did not contain any results.') != -1
-                    bad_response = data.find('<html>') != -1
-                    if not no_results and not bad_response:
-                        with open(t['filename'], 'wb') as file:
-                            file.write(response.content)
-                    elif no_results:
-                        click.echo("\n{} failed.\nThe query you have run did not contain any results.\n".format(target_url))
-                    elif bad_response:
-                        click.echo("\n{} failed.\bBad response from the EdSight server.\n".format(target_url))
-                    else:
-                        click.echo("\n{} failed.\bSomething unexpected happened.".format(target_url))
+            ATTEMPTS = 0
+            STATUS_CODE = 0
+            data = '<html>'
+            while ATTEMPTS < 4 and STATUS_CODE != 200 and data.find('<html>') != -1:
+                try:
+                    response = s.get(t['url'], params=t['param'])
+                except Exception as e:
+                    click.echo(e)
+                    STATUS_CODE = 0
+                    continue
+                STATUS_CODE = response.status_code
+                data = response.text
+                target_url = response.url
+            if save and STATUS_CODE == 200:
+                # Lets check to make sure that the content is an actual CSV files with results
+                no_results = data.find('The query you have run did not contain any results.') != -1
+                bad_response = data.find('<html>') != -1
+                if not no_results and not bad_response:
+                    with open(t['filename'], 'wb') as file:
+                        file.write(response.content)
+                        click.echo('Saving {} on try: {}\n'.format(os.path.basename(t['filename']), ATTEMPTS))
+                elif no_results:
+                    click.echo("\n{} failed.\nThe query you have run did not contain any results.\n".format(target_url))
+                elif bad_response:
+                    click.echo("\n{} failed.\bBad response from the EdSight server.\n".format(target_url))
                 else:
-                    click.echo("We had an issue with this dataset. Please try again.")
+                    click.echo("\n{} failed.\bSomething unexpected happened.".format(target_url))
+            else:
+                click.echo("We had an issue with this dataset. Please try again.")

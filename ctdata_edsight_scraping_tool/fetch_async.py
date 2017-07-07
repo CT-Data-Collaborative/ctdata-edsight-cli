73844761a458c161e1800359bd9b54395169ed0c
Sasha Cuerda <sasha.cuerda@gmail.com>
2017-07-07 09:35:37 -0400

    added in async client error exception handling to deal with edsight issues

--- ctdata_edsight_scraping_tool/fetch_async.py
+++ ctdata_edsight_scraping_tool/fetch_async.py
@@ -35,34 +35,39 @@ HEADERS = {
 
 async def get_report(url, params, file, save):
     async with sema:
-        async with aiohttp.ClientSession() as session:
-            data = '<html>'
-            tries = 0
-            target_url = ''
-            while tries < 4 and data.find('<html>') != -1:
-                if tries > 0:
-                    click.echo("Try #{} for fetching {}".format(tries+1, target_url))
-                    time.sleep(.75)
-                async with session.get(BASE_URL, headers=HEADERS) as context:
-                    pass
-                async with session.get(url, headers=HEADERS, params=params) as resp:
-                    data = await resp.text()
-                    target_url = resp.url
-                tries += 1
-            if save:
-                no_results = (data.find('No Search Results') != -1) or (data.find('The query you have run did not contain any results.') != -1)
-                bad_response = data.find('<head>') != -1
-                if not no_results and not bad_response:
-                    async with aiofiles.open(file, 'w') as f:
-                        click.echo('Saving {} on try: {}\n'.format(os.path.basename(file), tries+1))
-                        await f.write(data)
-                elif no_results:
-                    click.echo("\n{} failed.\nThe query you have run did not contain any results.\n".format(target_url))
-                elif bad_response:
-                    click.echo("\n{} failed.\bBad response from the EdSight server.\n".format(target_url))
-                else:
-                    click.echo("\n{} failed.\bSomething unexpected happened.".format(target_url))
-
+        scraped = False
+        while not scraped:
+            try:
+                async with aiohttp.ClientSession() as session:
+                    data = '<html>'
+                    tries = 0
+                    target_url = ''
+                    while tries < 4 and data.find('<html>') != -1:
+                        if tries > 0:
+                            click.echo("Try #{} for fetching {}".format(tries+1, target_url))
+                            time.sleep(.75)
+                        async with session.get(BASE_URL, headers=HEADERS) as context:
+                            pass
+                        async with session.get(url, headers=HEADERS, params=params) as resp:
+                            data = await resp.text()
+                            target_url = resp.url
+                        tries += 1
+                    if save:
+                        no_results = (data.find('No Search Results') != -1) or (data.find('The query you have run did not contain any results.') != -1)
+                        bad_response = data.find('<head>') != -1
+                        if not no_results and not bad_response:
+                            async with aiofiles.open(file, 'w') as f:
+                                click.echo('Saving {} on try: {}\n'.format(os.path.basename(file), tries+1))
+                                await f.write(data)
+                        elif no_results:
+                            click.echo("\n{} failed.\nThe query you have run did not contain any results.\n".format(target_url))
+                        elif bad_response:
+                            click.echo("\n{} failed.\bBad response from the EdSight server.\n".format(target_url))
+                        else:
+                            click.echo("\n{} failed.\bSomething unexpected happened.".format(target_url))
+                    scraped = True
+            except aiohttp.client_exceptions.ClientOSError as e:
+                scraped = False
 
 def fetch_async(dataset, output_dir, geography, catalog, save=True):
     targets = _setup_download_targets(dataset, output_dir, geography, catalog)
